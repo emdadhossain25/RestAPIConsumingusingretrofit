df8409be05f93a893ec0cb0cbd2f1e108525a2eb
emdad.hossain25 <emdad.hossain321@gmail.com>
2017-04-08 03:26:17 +0600

    part 1

---
+++ app/src/main/java/jp/co/jmas/restapiconsumingusingretrofit/JSONResponse.java
@@ -0,0 +1,13 @@
+package jp.co.jmas.restapiconsumingusingretrofit;
+
+/**
+ * Created by Administrator on 4/8/2017.
+ */
+
+public class JSONResponse {
+    private Movie[] android;
+
+    public Movie[] getAndroid() {
+        return android;
+    }
+}
--- app/src/main/java/jp/co/jmas/restapiconsumingusingretrofit/MainActivity.java
+++ app/src/main/java/jp/co/jmas/restapiconsumingusingretrofit/MainActivity.java
@@ -2,12 +2,50 @@ package jp.co.jmas.restapiconsumingusingretrofit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private MoviesAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        mRecyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+        mAdapter = new MoviesAdapter(this);
+        mRecyclerView.setAdapter(mAdapter);
+        List<Movie> movies = new ArrayList<>();
+
+        for (int i = 0; i < 25; i++) {
+            movies.add(new Movie());
+        }
+        mAdapter.setMovieList(movies);
+
+
     }
+
+    public static class MovieViewHolder extends RecyclerView.ViewHolder {
+        public ImageView imageView;
+
+        public MovieViewHolder(View itemView) {
+            super(itemView);
+            imageView = (ImageView) itemView.findViewById(R.id.imageView);
+        }
+    }
+
 }
